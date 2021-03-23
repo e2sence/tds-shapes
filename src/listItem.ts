9cfa9affaabb8b84f9c92faac82b83796e2a4b00
e2sence <32023734+e2sence@users.noreply.github.com>
2021-03-23 16:48:49 +0300

    init

---
+++ src/listItem.ts
@@ -0,0 +1,5 @@
+export class listItem {}
+
+export const li = () => {
+  console.log('ell')
+}
--- tds-shapes-entry.ts
+++ tds-shapes-entry.ts
@@ -5,5 +5,9 @@ export * from './src/style'
 export * from './src/title'
 export * from './src/background'
 export * from './src/label'
+
 export * from './src/textbox'
+
 export * from './src/slider'
+
+export * from './src/listItem'
