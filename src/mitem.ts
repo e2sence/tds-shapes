9de84025e2ceb1c49f50cbf61b4b1cfb73595344
e2sence <32023734+e2sence@users.noreply.github.com>
2021-04-01 09:03:46 +0300

    asd

--- src/mitem.ts
+++ src/mitem.ts
@@ -34,37 +34,26 @@ export const mitemCreator = (
       indents: [4, 2, 4, 2],
       position: { x: p.x, y: p.y },
     },
-    {
-      border: {
-        width: 1,
-        height: 1,
-        fill: { color: 'transparent' },
-        stroke: { color: '#3F8EFC', width: 1, dasharray: '5 5' },
-        radius: 3,
-      },
-      indents: [2, 2, 2, 2],
-    },
     9
   )
 }
 
 const MITEM_FRIENDS_ZONE = 200
-const GRID_STEP = 9
+const GRID_STEP = 18
 
 /** base item for tds-core */
 export class mitem extends label {
-  outline: mitemOutline
   widthFactor: number
 
-  fromFreeDrag: boolean = true
   snaped: boolean = false
 
   constructor(
     attr: LabelAttr,
-    outline: { border: BackgroundStyle; indents: Indents },
+
     wFactor: number
   ) {
     super(attr)
+
     this.id(Create_ID()).addClass('tds-mitem')
 
     this.widthFactor = wFactor
@@ -73,28 +62,18 @@ export class mitem extends label {
 
     // set initial position to grid
     const bb = this.background.bbox()
-    let tx = bb.x - (bb.x % this.widthFactor) + attr.indents[1]
-    let ty = bb.y - (bb.y % this.widthFactor) + attr.indents[3]
+    let tx = bb.x - (bb.x % this.widthFactor)
+    let ty = bb.y - (bb.y % this.widthFactor)
     this.move(tx, ty)
 
-    // set outline
-    this.outline = new mitemOutline(outline.border, outline.indents)
-    this.add(this.outline)
-    this.outline.hide()
     this.on('mouseenter', () => {
-      this.outline.show()
+      this.background.stroke({ color: '#E8871E', width: 2 })
+      this.front()
     })
-    this.on('touchstart', () => {
-      console.log('ts')
 
-      this.outline.show()
-    })
     this.on('mouseleave', () => {
-      this.outline.hide()
+      this.background.stroke({ color: 'black', width: 1 })
     })
-
-    // correct outline
-    this.setOutline()
 
     this.on('dragmove', (ev: CustomEvent) => {
       snapHandler(ev, this)
@@ -127,13 +106,8 @@ export class mitem extends label {
                   )
                   // turn on snap to grid mode
                   if (adist < inst.widthFactor) {
-                    let dx = this_el[0] - c_el[0]
-                    let dy = this_el[1] - c_el[1]
-                    let q = inst.getQuater(dx, dy)
-                    console.log(q)
-
-                    ev.preventDefault()
                     const { box } = ev.detail
+                    ev.preventDefault()
 
                     if (!inst.snaped) {
                       inst.move(
@@ -150,8 +124,9 @@ export class mitem extends label {
             }
           })
       } else {
-        ev.preventDefault()
         const { box } = ev.detail
+        ev.preventDefault()
+
         inst.move(
           box.x - (box.x % inst.widthFactor),
           box.y - (box.y % inst.widthFactor)
@@ -160,40 +135,13 @@ export class mitem extends label {
     }
 
     this.on('dragend', () => {
-      //   if (this.fromFreeDrag) {
-      // set position to grid
       const box = this.background.bbox()
       this.move(
         box.x - (box.x % this.widthFactor),
         box.y - (box.y % this.widthFactor)
       )
       this.snaped = false
-      //   } else {
-      //     this.fromFreeDrag = true
-      //   }
     })
-  }
-
-  /**
-   *          41
-   *        4 | 1
-   *     34 ----- 12
-   *        3 | 2
-   *          23
-   */
-  /** control of the quarter approach to the goal */
-  getQuater(dx: number, dy: number) {
-    if (dx > 0 && dy < 0) return 1
-    if (dx > 0 && dy > 0) return 2
-    if (dx < 0 && dy > 0) return 3
-    if (dx < 0 && dy < 0) return 4
-    if (dx > 0 && dy == 0) return 12
-    if (dx == 0 && dy > 0) return 23
-    if (dx < 0 && dy == 0) return 34
-    if (dx == 0 && dy < 0) return 41
-    if (dx == 0 && dy == 0) return 5
-
-    return 5
   }
 
   /**  correct width according to widthFactor */
@@ -213,8 +161,6 @@ export class mitem extends label {
     this.value = v
     // correct width according to width factor
     this.correctWidth()
-
-    this.setOutline()
   }
 
   /** stick points */
@@ -230,39 +176,5 @@ export class mitem extends label {
       [bb.x + bb.width / 2, bb.y2],
       [bb.x, bb.y2],
     ]
-  }
-
-  /** set outline size and position according to its indents  */
-  setOutline() {
-    let bb = this.background.bbox()
-    let b = this.outline
-
-    b.border.width(bb.width + b.indents[0] + b.indents[2])
-    b.border.height(bb.height + b.indents[1] + b.indents[3])
-
-    b.x(bb.x - b.indents[0])
-    b.y(bb.y - b.indents[0])
-  }
-}
-
-/** the stroke line is activated when you hover over an object */
-export class mitemOutline extends G {
-  /** outline style */
-  border: Rect
-  /** padding from parent object */
-  indents: Indents
-
-  /** create outline instance */
-  constructor(attr: BackgroundStyle, i: Indents) {
-    super()
-
-    this.indents = i
-
-    this.border = new Rect()
-      .fill({ ...attr.fill })
-      .stroke({ ...attr.stroke })
-      .radius(attr.radius)
-
-    this.add(this.border)
   }
 }
